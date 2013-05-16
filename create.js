ce92f37278d36002a84acb10307f772eac049d90
Loginov Andrew <mrlogaz@gmail.com>
2013-05-16 14:34:16 +0600

    start :)

---
+++ create.js
@@ -0,0 +1,351 @@
+var Create = {};
+
+Create.graph = [
+    {
+      "level": 0.5,
+      "time": "06:00"
+    },
+    {
+      "level": 0.3,
+      "time": "06:10"
+    },
+    {
+      "level": 1.0,
+      "time": "06:20"
+    },
+    {
+      "level": 0.8,
+      "time": "06:30"
+    },
+    {
+      "level": 0.8,
+      "time": "06:40"
+    },
+    {
+      "level": 0.7,
+      "time": "06:50"
+    },
+    {
+      "level": 1.0,
+      "time": "07:00"
+    },
+    {
+      "level": 1.5,
+      "time": "07:10"
+    },
+    {
+      "level": 2.6,
+      "time": "07:20"
+    },
+    {
+      "level": 3.0,
+      "time": "07:30"
+    },
+    {
+      "level": 3.0,
+      "time": "07:40"
+    },
+    {
+      "level": 3.5,
+      "time": "07:50"
+    },
+    {
+      "level": 3.8,
+      "time": "08:00"
+    },
+    {
+      "level": 3.1,
+      "time": "08:10"
+    },
+    {
+      "level": 3.56,
+      "time": "08:20"
+    },
+    {
+      "level": 4.0,
+      "time": "08:30"
+    },
+    {
+      "level": 4.86,
+      "time": "08:40"
+    },
+    {
+      "level": 4.67,
+      "time": "08:50"
+    },
+    {
+      "level": 4.17,
+      "time": "09:00"
+    },
+    {
+      "level": 4.2,
+      "time": "09:10"
+    },
+    {
+      "level": 4.17,
+      "time": "09:20"
+    },
+    {
+      "level": 4.0,
+      "time": "09:30"
+    },
+    {
+      "level": 4.0,
+      "time": "09:40"
+    },
+    {
+      "level": 4.0,
+      "time": "09:50"
+    },
+    {
+      "level": 4.0,
+      "time": "10:00"
+    },
+    {
+      "level": 4.0,
+      "time": "10:10"
+    },
+    {
+      "level": 4.0,
+      "time": "10:20"
+    },
+    {
+      "level": 4.0,
+      "time": "10:30"
+    },
+    {
+      "level": 4.0,
+      "time": "10:40"
+    },
+    {
+      "level": 4.0,
+      "time": "10:50"
+    },
+    {
+      "level": 4.4,
+      "time": "11:00"
+    },
+    {
+      "level": 4.0,
+      "time": "11:10"
+    },
+    {
+      "level": 4.4,
+      "time": "11:20"
+    },
+    {
+      "level": 4.2,
+      "time": "11:30"
+    },
+    {
+      "level": 4.0,
+      "time": "11:40"
+    },
+    {
+      "level": 4.2,
+      "time": "11:50"
+    },
+    {
+      "level": 5.0,
+      "time": "12:00"
+    },
+    {
+      "level": 4.2,
+      "time": "12:10"
+    },
+    {
+      "level": 4.2,
+      "time": "12:20"
+    },
+    {
+      "level": 4.2,
+      "time": "12:30"
+    },
+    {
+      "level": 4.0,
+      "time": "12:40"
+    },
+    {
+      "level": 4.0,
+      "time": "12:50"
+    },
+    {
+      "level": 4.0,
+      "time": "13:00"
+    },
+    {
+      "level": 4.4,
+      "time": "13:10"
+    },
+    {
+      "level": 4.8,
+      "time": "13:20"
+    },
+    {
+      "level": 4.6,
+      "time": "13:30"
+    },
+    {
+      "level": 4.0,
+      "time": "13:40"
+    },
+    {
+      "level": 4.4,
+      "time": "13:50"
+    },
+    {
+      "level": 4.2,
+      "time": "14:00"
+    },
+    {
+      "level": 4.8,
+      "time": "14:10"
+    },
+    {
+      "level": 4.6,
+      "time": "14:20"
+    },
+    {
+      "level": 5.0,
+      "time": "14:30"
+    },
+    {
+      "level": 5.0,
+      "time": "14:40"
+    },
+    {
+      "level": 5.0,
+      "time": "14:50"
+    },
+    {
+      "level": 5.0,
+      "time": "15:00"
+    },
+    {
+      "level": 5.0,
+      "time": "15:10"
+    },
+    {
+      "level": 5.0,
+      "time": "15:20"
+    },
+    {
+      "level": 5.0,
+      "time": "15:30"
+    },
+    {
+      "level": 5.0,
+      "time": "15:40"
+    },
+    {
+      "level": 5.0,
+      "time": "15:50"
+    },
+    {
+      "level": 5.0,
+      "time": "16:00"
+    },
+    {
+      "level": 5.0,
+      "time": "16:10"
+    },
+    {
+      "level": 5.0,
+      "time": "16:20"
+    },
+    {
+      "level": 5.0,
+      "time": "16:30"
+    },
+    {
+      "level": 5.0,
+      "time": "16:40"
+    },
+    {
+      "level": 5.0,
+      "time": "16:50"
+    },
+    {
+      "level": 5.0,
+      "time": "17:00"
+    },
+    {
+      "level": 5.0,
+      "time": "17:10"
+    },
+    {
+      "level": 5.6,
+      "time": "17:20"
+    },
+    {
+      "level": 6.0,
+      "time": "17:30"
+    },
+    {
+      "level": 6.0,
+      "time": "17:40"
+    },
+    {
+      "level": 6.0,
+      "time": "17:50"
+    },
+    {
+      "level": 6.0,
+      "time": "18:00"
+    },
+    {
+      "level": 6.2,
+      "time": "18:10"
+    },
+    {
+      "level": 6.6,
+      "time": "18:20"
+    },
+    {
+      "level": 6.0,
+      "time": "18:30"
+    },
+    {
+      "level": 6.0,
+      "time": "18:40"
+    },
+    {
+      "level": 6.0,
+      "time": "18:50"
+    }
+  ]
+
+Create.initialize = function() {
+  Create.vars = {};
+  Create.vars.canvas =  document.getElementById('GraphCanvas'); // Получаем холст с заданным ID
+  Create.vars.ctx = Create.vars.canvas.getContext('2d');
+  Create.vars.canvas.height = Create.vars.canvas.height; // Высота холста
+  Create.vars.canvas.width = Create.vars.canvas.width;  // Ширина холста
+  console.log(this);
+  Create.ready();
+};
+
+Create.ready = function() {
+  Create.readyGraph = {};
+  Create.readyGraph.stepY = 70;
+  Create.readyGraph.stepX = Create.vars.canvas.width / Create.graph.length;
+  console.log(Create.graph.length);
+  for (var i = 0; Create.graph.length > i; i++) {
+    Create.draw(i * Create.readyGraph.stepX, Create.vars.canvas.height - (Create.graph[i].level * Create.readyGraph.stepY));
+  };
+};
+
+Create.draw = function(gX,gY) {
+  ctx = Create.vars.ctx;
+  ctx.lineCap = 'round';
+  ctx.beginPath();
+  ctx.moveTo(Create.oldX, Create.oldY);
+  ctx.lineTo(gX - 2, gY);
+  ctx.lineJoin = 'round';
+  ctx.strokeStyle = 'red';
+  ctx.lineWidth = 20;
+  ctx.stroke();
+  console.log(gX);
+  Create.oldX = gX;
+  Create.oldY = gY;
+};
